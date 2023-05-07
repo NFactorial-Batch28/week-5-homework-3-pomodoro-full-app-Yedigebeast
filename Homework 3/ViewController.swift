8d3e895482634c20818954c5b220d47c7c4165f8
Yedige Ashirbek <edige11020@gmail.com>
2023-05-07 18:38:27 +0600

    Initial Commit

---
+++ Homework 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 3
+//
+//  Created by Yedige Ashirbek on 07.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
